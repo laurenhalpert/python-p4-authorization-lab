1f9390210aaeabe67965c261d62f463cde74d753
Lauren <halpertln@gmail.com>
2023-07-21 12:33:23 -0600

    complete lab

--- server/app.py
+++ server/app.py
@@ -3,10 +3,11 @@
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
+# from flask_cors import CORS
 from models import db, Article, User
 
 app = Flask(__name__)
+
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -17,6 +18,21 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+
+@app.before_request
+def check_if_logged_in():
+    open_list = [
+        'clear',
+        'article_list',
+        'show_article',
+        'check_session',
+        'login',
+        'logout'
+    ]
+    if (request.endpoint) not in open_list and (not session('user_id')) :
+        return {'error': 'Unauthorized'}, 401
+
 
 class ClearSession(Resource):
 
@@ -87,12 +103,14 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
